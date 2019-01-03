b8d3bfbd438b53af5ebfc7ae74344343f261c1fd
GimunLee <gimoon0226@naver.com>
2019-01-03 12:54:29 +0900

    Learn about Using Array and Method

---
+++ FirstProject/src/com/array/ArrayTest2.java
@@ -0,0 +1,18 @@
+package com.array;
+
+public class ArrayTest2 {
+	
+	public static void main(String[] args) {
+		int[] score = {50,90,88,100,50};
+		int[] code = new int[]{2,4,5,6,7};
+		
+/*		for(String data : args) {
+			System.out.println(data);
+		}
+		
+		int x = Integer.parseInt(args[0]);
+		int y = Integer.parseInt(args[1]);
+		
+		System.out.println(x + y);*/
+	}
+}
