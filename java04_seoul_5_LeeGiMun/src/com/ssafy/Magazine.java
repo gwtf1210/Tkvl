bd433bc463deeaa3a7b377fdcce3a7b0566d79c8
GimunLee <gimoon0226@naver.com>
2019-01-10 17:38:30 +0900

    Workshop in SSAFY, Thurs of 5th week

--- java04_seoul_5_LeeGiMun/src/com/ssafy/Magazine.java
+++ java04_seoul_5_LeeGiMun/src/com/ssafy/Magazine.java
@@ -1,14 +1,14 @@
 package com.ssafy;
 
 public class Magazine {
-	String isbn;
-	String title;
-	String author;
-	String publisher;
-	int year;
-	int month;
-	int price;
-	String desc;
+	private String isbn;
+	private String title;
+	private String author;
+	private String publisher;
+	private int year;
+	private int month;
+	private int price;
+	private String desc;
 	
 	public Magazine(String isbn, String title, String author, String publisher, int year, int month, int price,
 			String desc) {
@@ -24,6 +24,86 @@ public class Magazine {
 	}
 	
 	
+	public String getIsbn() {
+		return isbn;
+	}
+
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getAuthor() {
+		return author;
+	}
+
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+
+	public int getYear() {
+		return year;
+	}
+
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+
+	public int getMonth() {
+		return month;
+	}
+
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+	public String getDesc() {
+		return desc;
+	}
+
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+
 	public String toString() {
 		return isbn + " | " + title + " | " +  author + " | " +  publisher + " | " +  price + " | " +  desc + " | " + year + "." + month;
 	}
