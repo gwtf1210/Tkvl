d100bd341070bf6198a1cc90e6a4b5403e982139
GimunLee <gimoon0226@naver.com>
2019-01-10 17:37:58 +0900

    Workshop in SSAFY, Thurs of 5th week

--- SecondProject/src/com/subway/Order.java
+++ SecondProject/src/com/subway/Order.java
@@ -1,11 +1,7 @@
 package com.subway;
 
 public class Order {
-	
-	private String orderPaper;
-	
 	public static void main(String[] args) {
-		
 		// 에그마요 주문
 		Sandwich sw1 = new EggMayo(1, "에그마요","","360kcal","16000원","Large","JMT 입니다.", "50kg"); 
 		Sandwich sw2 = new BLT(2, "BLT","","600kcal","10300원","Large","별로 맛 없습니다..", "150kg"); 
@@ -15,8 +11,5 @@ public class Order {
 		sw1.make();
 		sw2.make();
 		sw3.make();
-		
-		
-		
 	}
 }
