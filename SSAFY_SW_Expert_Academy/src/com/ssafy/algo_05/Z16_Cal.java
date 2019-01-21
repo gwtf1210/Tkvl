61fef3658cd9b45b3547020c36f7f176f218d10f
GimunLee <gimoon0226@naver.com>
2019-01-21 13:04:06 +0900

    Solve Calculator Problem

---
+++ SSAFY_SW_Expert_Academy/src/com/ssafy/algo_05/Z16_Cal.java
@@ -0,0 +1,42 @@
+package com.ssafy.algo_05;
+
+/**
+ * °è»ê±â 
+ * 
+ * input
+ * 31 + 4 * 2 + 7 * 2
+ */
+
+public class Z16_Cal {
+	public static void main(String[] args) {
+		String inputLine = "31 + 4 * 2 + 7 * 2";
+		
+		String[] stack = new String[20];
+		int top = -1;
+		
+		String[] input = inputLine.split(" ");
+		
+		for (int i = 0; i < input.length; i++) {
+			stack[++top] = input[i];
+			if(stack[top].equals("*")) {
+				int A = Integer.parseInt(stack[--top]);
+				int B = Integer.parseInt(input[i+1]);
+				stack[top] = (A*B) + "";
+				i++;
+			}
+			
+			
+		}
+		
+		for (int i = top; i > -1; i--) {
+			if(stack[i].equals("+")) {
+				int A = Integer.parseInt(stack[i+1]);
+				int B = Integer.parseInt(stack[i-1]);
+				top-=2;
+				stack[top] = (A+B) +"";
+			}
+		}
+		System.out.println(stack[0]);	
+		
+	} // end of main
+} // end of class
