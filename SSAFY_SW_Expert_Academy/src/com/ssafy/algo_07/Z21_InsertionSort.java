6da64152400255af48779768f76590db85fa2662
GimunLee <gimoon0226@naver.com>
2019-01-28 10:18:04 +0900

    삽입 정렬 개선 후

--- SSAFY_SW_Expert_Academy/src/com/ssafy/algo_07/Z21_InsertionSort.java
+++ SSAFY_SW_Expert_Academy/src/com/ssafy/algo_07/Z21_InsertionSort.java
@@ -10,20 +10,21 @@ public class Z21_InsertionSort {
 //		정렬된 묶음 : 0
 //		정렬안된 묶음 : 1 ~ arr.length -1
 		// 정렬 안된 데이터에서 하나씩 꺼내기
-		
 		for (int i = 1; i < arr.length; i++) { // 비정렬 원소
 			// 삽입할 위치를 찾아 넣기
-			for (int j = i-1; j >= 0; j--) { // 뒤쪽에서 앞으로
-				if(arr[j] > arr[j+1]) {
-					int temp = arr[j+1];
+			int num = arr[i];
+			int j;
+			for (j = i-1; j >= 0; j--) { // 뒤쪽에서 앞으로
+				if(arr[j] > num) {
+//					int temp = arr[j+1];
 					arr[j+1] = arr[j];
-					arr[j] = temp;
+//					arr[j] = temp;
 				}else { 
 					break; // 성능을 높일 수 있다.
 				}
-				System.out.println(Arrays.toString(arr));
 			}
+			arr[j+1] = num;
 		}
-
+		System.out.println(Arrays.toString(arr));
 	} // end of main
 } // end of class
