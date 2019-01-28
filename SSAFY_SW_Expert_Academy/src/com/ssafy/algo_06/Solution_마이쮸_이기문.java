bda7f1a1145d86e3b9bbcbd1755faabe41816a15
GimunLee <gimoon0226@naver.com>
2019-01-28 09:21:09 +0900

    Change Variable name

--- SSAFY_SW_Expert_Academy/src/com/ssafy/algo_06/Solution_마이쮸_이기문.java
+++ SSAFY_SW_Expert_Academy/src/com/ssafy/algo_06/Solution_마이쮸_이기문.java
@@ -8,19 +8,19 @@ public class Solution_마이쮸_이기문 {
 		int front = -1;
 		int rear = -1;
 		int myChoo = 20;
-		int human[] = new int[50]; // 0은 안씀
+		int each_myChoo[] = new int[50]; // 0은 안씀
 		int index = 1;
 		
 		// 처음 inQueue
 		queue[++rear] = index;
-		human[index]++; // 0 1
+		each_myChoo[index]++; // 0 1
 		
 		while(myChoo >  0) { // 종료 조건 : 마이쮸가 0개일때
 			queue[++rear]= queue[++front]; // 디큐한것을 인큐한다
-			myChoo -= human[queue[front]];
-			human[queue[front]]++;
+			myChoo -= each_myChoo[queue[front]];
+			each_myChoo[queue[front]]++;
 			queue[++rear] = (++index); //사람 늘리기
-			human[index]++;
+			each_myChoo[index]++;
 		}
 		System.out.println("마지막 마이쮸 : " + queue[front]);
 	}
