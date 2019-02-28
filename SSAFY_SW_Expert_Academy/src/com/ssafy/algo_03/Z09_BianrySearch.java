330f84aa1bea8c675b0a800ed651b50d4a58eaf8
GimunLee <gimoon0226@naver.com>
2019-02-28 10:40:03 +0900

    Change error

--- SSAFY_SW_Expert_Academy/src/com/ssafy/algo_03/Z09_BianrySearch.java
+++ SSAFY_SW_Expert_Academy/src/com/ssafy/algo_03/Z09_BianrySearch.java
@@ -11,15 +11,15 @@ import java.util.Arrays;
 
 public class Z09_BianrySearch {
 	public static void main(String[] args) {
-		int a[] = {0,1,2,3,4,5,6,7,8,9};
-		int key = 8; // 찾으려는 데이터
-//		binarySearch(a, 0, a.length-1, key);
+		int a[] = {0,1,2,3,4,5,6,7,8,9,10};
+		int key = 11; // 찾으려는 데이터
+		binarySearch(a, 0, a.length-1, key);
 		
-		if(Arrays.binarySearch(a, key) >= 0) {
-			System.out.println("찾음");
-		}else {
-			System.out.println("못 찾음");
-		}
+//		if(Arrays.binarySearch(a, key) >= 0) {
+//			System.out.println("찾음");
+//		}else {
+//			System.out.println("못 찾음");
+//		}
 		
 //		int l = 0;
 //		int r = a.length-1;
@@ -36,6 +36,7 @@ public class Z09_BianrySearch {
 //			}
 //		}
 //		if(l > r) { // While의 거짓 조건
+//			System.out.println(a[l]);
 //			System.out.println("없음");
 //		}
 	} // end of main
@@ -46,13 +47,13 @@ public class Z09_BianrySearch {
 			System.out.println("검색 실패");
 			return;
 		}else {
-			int mid = l + r/2; // 중간 인덱스
+			int mid = (l + r)/2; // 중간 인덱스
 			if(key == a[mid]) {
 				System.out.println("찾음");
 				return;
 			}else if(key < a[mid]){ // 작은쪽으로 범위 재설정
 				binarySearch(a, l, mid-1, key);
-			}else { // if(key > a[mid]) // 큰 족으로 범위 재설정
+			}else { // if(key > a[mid]) // 큰 쪽으로 범위 재설정
 				binarySearch(a, mid+1, r, key);				
 			}
 		}
